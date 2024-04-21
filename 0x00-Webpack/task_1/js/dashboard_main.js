af4dc0c3b16239f083abae6d151fedb4857fbf1b
vierukundo <vierukundo@gmail.com>
2024-04-22 00:10:53 +0200

    configured webpack config file

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,21 @@
+import $ from "jquery";
+import _ from "lodash";
+
+$(function(){
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<button>Click here to get started</button>');
+    $('body').append("<p id='count'></p>");
+    $('body').append("<p>Copyright - Holberton School</p>");
+
+    let count = 0;
+
+    function updateCounter() {
+        $('button').on('click', _.debounce(() => {
+            count += 1;
+            $('#count').text(`${count} clicks on the button`);
+        }, 300));
+    }
+
+    updateCounter();
+});
